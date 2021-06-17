db3cf29e5c8f4255927fe719428c3d4e59c0e9e1
Eduardo Aguilar <eaguilar92105@yahoo.com>
2021-06-16 19:32:09 -0700

    created message components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import { ChatEngine } from 'react-chat-engine';
 import './App.css';
+import ChatFeed from './components/ChatFeed';
 
 const App = () => {
   return (
     <ChatEngine
       height="100vh"
-
       projectID={process.env.REACT_APP_PROJECT_ID}
       userName={process.env.REACT_APP_USER_NAME}
       userSecret={process.env.REACT_APP_USER_SECRET}
+      renderChatFeed={(chatAppProps) =><ChatFeed {...chatAppProps} /> }
     />
   )
-     
 }
 
 export default App;
